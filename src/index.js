017d54368456b19e1592dd14c92626c1aa2d1bf8
Murat Benli <muratbenli001@gmail.com>
2020-03-28 12:30:05 +0300

    add video select func

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = '';
+const API_KEY = 'AIzaSyC2fPqJPBNyvyV_3PuUpsL_BfzdOoje1qg';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: []};
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
-            // this.setState({ videos: videos})
+        YTSearch({key: API_KEY, term: 'Post Malone'}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            })
         });
     }
 
@@ -23,8 +28,11 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}    
+                    videos={this.state.videos} 
+                />
             </div>
         );
     }
